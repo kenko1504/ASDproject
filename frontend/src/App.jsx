47f6b57feeceaf7a4c358cfdd4e3dd841e44a0e2
Kenji Watanabe <kenkowatanabe1504@gmail.com>
2025-08-28 14:35:11 +1000

    main page layoutn

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,9 @@
 import { useState, useEffect } from "react";
 import AddIngredient from "./AddIngredient";
 import Header from "./Elements/Header";
+import "./CSS/Dashboard.css";
 
+// entry point
 export default function App() {
   const [ingredients, setIngredients] = useState([]);
 
@@ -18,25 +20,20 @@ export default function App() {
 
   return (
     <>
-      <Header></Header>
+      <div className="container">
+        <div className="sidebar">
+          <h1>FridgeManager</h1>
+          <button>Your Fridge</button>
+          <button>Nutritions</button>
+          <button>Waste and Budget Control</button>
+          <button>Your Recipes</button>
+        </div>
 
-  
-      <div>
-        <h1>Ingredients</h1>
-
-        <AddIngredient onAdd={handleAdd} />
-
-        <ul>
-          {ingredients.map(item => (
-            <li key={item._id}>
-              {item.name} - {item.quantity}
-            </li>
-          ))}
-        </ul>
-      </div>        
-
-      
+        <div className="dashboard">
+          <h2>Dashboard</h2>
+          {/* Your dashboard content here */}
+        </div>
+      </div>
     </>
-    
   );
 }
