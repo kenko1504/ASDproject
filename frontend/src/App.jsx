b7733828c7d7254618f0024a564fd5d2870c602f
Jianan617 <huangjianan203617@gmail.com>
2025-09-01 20:15:55 +1000

    put all components in the components file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,8 @@ import ItemManagement from "./components/ItemManagement";
 import Nutrition from "./components/Nutritions";
 import WasteBudget from "./components/WasteBudget";
 import Settings from "./components/Settings";
-import Sidebar from "./Sidebar";
+import Sidebar from "./components/DashboardComponent/Sidebar.jsx";
+import FridgeList from "./components/FridgeList.jsx";
 
 export default function App() {
     return (
@@ -17,10 +18,10 @@ export default function App() {
             <Sidebar />
             <main className="content">
                 <Routes>
-                    <Route path="/" element={<Login />} />
-                    <Route path="/dashboard" element={<Dashboard ingredients={ingredients} />} />
+                    {/*<Route path="/" element={<Login />} />*/}
+                    <Route path="/" element={<Dashboard/>} />
                     <Route path="/fridge" element={<FridgeList />} />
-                    <Route path="/item-management" element={<ItemManagement ingredients={ingredients}/>} />
+                    <Route path="/item-management" element={<ItemManagement/>} />
                     <Route path="/nutrition" element={<Nutrition />} />
                     <Route path="/waste-budget" element={<WasteBudget />} />
                     <Route path="/recipes" element={<Recipes />} />
