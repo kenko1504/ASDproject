62bbed9d1b7871b808515f6eeacb033ab5c65395
Dablestar <jbynchoi@gmail.com>
2025-09-02 00:04:33 +1000

    fixed multiple declaration

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,16 +3,9 @@ function Header() {
 
     return(
         <header>
-            <div className='header-title'>
+            <div className='header-title '>
                 <h1>WEBSITE</h1>
             </div>
-
-            <nav>
-                <a href="#"><div className='header-button'>PAGE 1</div></a>
-                <a href="#"><div className='header-button'>PAGE 2</div></a>
-                <a href="#"><div className='header-button'>PAGE 3</div></a>
-                <a href="#"><div className='header-button'>PAGE 4</div></a>
-            </nav>
         </header>
     );
 }
