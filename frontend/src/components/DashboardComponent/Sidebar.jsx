37ba5cf360ef5c6fce8656d9fba08d4302ef38bd
Dablestar <jbynchoi@gmail.com>
2025-09-02 10:42:11 +1000

    added sidebar icons and layout almost done

--- frontend/src/components/DashboardComponent/Sidebar.jsx
+++ frontend/src/components/DashboardComponent/Sidebar.jsx
@@ -1,27 +1,36 @@
     import {Link} from "react-router-dom";
     import "../../index.css";
+    import logoImg from "../../assets/LogoIcon.png"
+    import signOut from "../../assets/SignOut.svg"
+    import sampleProfileImg from "../../assets/SampleProfilePic.jpg"
+    import settingImg from "../../assets/Settings.svg"
+    import notificationImg from "../../assets/Notification.svg"
 
     export default function Sidebar(){
         return (
             <div className={"sidebar w-[200px] flex flex-col bg-[#85BC59] p-5 min-h-screen"}>
-                <div className="logo cinzel-decorative-regular text-center text-xl mb-6">
-                    <svg>{/* Logo Icon Here*/}</svg>
-                    <span className={"title text-white weight-bold text-center"}><Link to="/">FridgeManager</Link></span>
+                <div className="logo flex cinzel-decorative-regular text-center text-xl !mt-5 content-center">
+                    <img className="w-2/12 h-8 mx-auto" src={logoImg} alt="logo" />
+                    <span className={"title w-8/12 text-white weight-bold text-center"}><Link to="/">FridgeManager</Link></span>
                 </div>
                 <nav className="flex flex-grow flex-col">
-                    <ul className="m-10 text-white font-bold text-center">
-                        <li className="h-12 flex items-center justify-center"><Link to={"/fridge"}>Your Fridge</Link></li>
-                        <li className="h-12 flex items-center justify-center"><Link to={"/item-management"}>Item Management</Link></li>
-                        <li className="h-12 flex items-center justify-center"><Link to={"/nutrition"}>Nutrition</Link></li>
-                        <li className="h-12 flex items-center justify-center"><Link to={"/wasteBudget"}>Waste and <br></br>Budget Control</Link></li>
+                    <ul className="!mt-15 text-white font-bold text-center">
+                        <li className="h-15 flex items-center justify-center transition hover:bg-[#6FAF4B]"><Link to={"/fridge"}>Your Fridge</Link></li>
+                        <li className="h-15 flex items-center justify-center transition hover:bg-[#6FAF4B]"><Link to={"/item-management"}>Item Management</Link></li>
+                        <li className="h-15 flex items-center justify-center transition hover:bg-[#6FAF4B]"><Link to={"/nutrition"}>Nutrition</Link></li>
+                        <li className="h-15 flex items-center justify-center transition hover:bg-[#6FAF4B]"><Link to={"/wasteBudget"}>Waste and <br></br>Budget Control</Link></li>
                     </ul>
                 </nav>
-                <div className={"sidebar-footer mt-auto"}>
-                    <button><svg></svg>{/*Notifications*/}</button><br/>
-                    <button><svg></svg>Settings</button><br/>
-                    <button><img src={"#"} alt={"ProfilePic"}/><span>{/*User Name*/}</span></button><br/>
-                    <button><svg></svg>{/*Sign Out*/}</button>
+                <div className={"sidebar-footer !mt-auto"}>
+                    <div className="upper flex">
+                        <button className="flex w-3/12 items-center"><img className="w-12 h-12" src={notificationImg} alt={"Notifications"}/></button><br/>
+                        <button className="flex w-9/12 items-center"><img className="w-12 h-12" src={settingImg} alt={"Settings"}/><span className="font-bold text-white">Settings</span></button><br/>
+                   </div>
+                    <div className="lower flex">
+                        <button className="flex w-3/12 items-center"><img className="w-12 h-12" src={signOut} alt={"Sign Out"}/></button>
+                        <button className="flex w-9/12 items-center"><img className="w-12 h-12 rounded-[50%]" src={sampleProfileImg} alt={"ProfilePic"}/><span className="font-bold text-white">UserName</span></button><br/>
                     </div>
+                </div>
             </div>
         )
     }
