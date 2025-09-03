98eaca46eeb405cc6ef90e5e02c61ec9dc072387
Anthony He <anthonyhe3154@gmail.com>
2025-09-04 01:44:17 +1000

    temp swap of groceryList schema model

--- backend/models/groceryList.js
+++ backend/models/groceryList.js
@@ -1,18 +1,30 @@
 import mongoose from "mongoose";
 
-const groceryListSchema = new mongoose.Schema({
-    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    items: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Item",
-        },
-    ],
-    status: { type: String, enum: ['pending', 'completed'], default: 'pending' },
-    createdAt: { type: Date, default: Date.now },
-    note: { type: String, default: '' },
-});
+const GroceryListSchema = new mongoose.Schema ( {
+    name: String, 
+    date: Date,
+    note: String,
+    status: String
+})
 
-const GroceryList = mongoose.model("GroceryList", groceryListSchema);
+const GroceryListModel = mongoose.model('GroceryList', GroceryListSchema);
+export default GroceryListModel;
 
-export default GroceryList;
+// import mongoose from "mongoose";
+
+// const groceryListSchema = new mongoose.Schema({
+//     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+//     items: [
+//         {
+//             type: mongoose.Schema.Types.ObjectId,
+//             ref: "Item",
+//         },
+//     ],
+//     status: { type: String, enum: ['pending', 'completed'], default: 'pending' },
+//     createdAt: { type: Date, default: Date.now },
+//     note: { type: String, default: '' },
+// });
+
+// const GroceryList = mongoose.model("GroceryList", groceryListSchema);
+
+// export default GroceryList;
--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import itemRoutes from "./routes/itemRoutes.js";
-import GroceryListModel from "./models/GroceryList.js";
+import GroceryListModel from "./models/groceryList.js";
 
 
 dotenv.config(); // read .env
