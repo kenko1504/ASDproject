aaf6d620370325a6bd1887c8b07fe1a2806f4b9d
Dablestar <jbynchoi@gmail.com>
2025-09-04 01:59:44 +1000

    added food.js schema for testing nutrition information display

--- frontend/src/components/Nutritions.jsx
+++ frontend/src/components/Nutritions.jsx
@@ -1,8 +1,22 @@
 export default function Nutritions() {
   return (
     <div>
-      <h2>Nutritions</h2>
-      {/* Your nutrition content here */}
+      <span className="title font-bold text-2xl">Nutritions</span>
+      <span className="search !ml-5">
+        <input type="text" placeholder="Type Food Name Here..." className="bg-gray-100 w-200" />
+      </span>
+      <div className="nutrition-info !mt-5">
+        <table>
+          <tr>
+            <th>Food Name</th>
+          </tr>
+          {
+            <tr>
+              <td><a href="/nutritions/apple">Apple</a></td>
+            </tr>
+          }
+        </table>
+      </div>
     </div>
   );
 }
