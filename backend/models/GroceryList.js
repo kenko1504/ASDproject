b38fb7d5cd59ab48c25668c054d8cfb4c025dbec
Bermisham <matthew.ewan.haywood@gmail.com>
2025-09-04 14:19:28 +1000

    Update GroceryList.js

--- backend/models/GroceryList.js
+++ backend/models/GroceryList.js
@@ -9,3 +9,22 @@ const GroceryListSchema = new mongoose.Schema ( {
 
 const GroceryListModel = mongoose.model('GroceryList', GroceryListSchema);
 export default GroceryListModel;
+
+// import mongoose from "mongoose";
+
+// const groceryListSchema = new mongoose.Schema({
+//     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+//     items: [
+//         {
+//             type: mongoose.Schema.Types.ObjectId,
+//             ref: "Item",
+//         },
+//     ],
+//     status: { type: String, enum: ['pending', 'completed'], default: 'pending' },
+//     createdAt: { type: Date, default: Date.now },
+//     note: { type: String, default: '' },
+// });
+
+// const GroceryList = mongoose.model("GroceryList", groceryListSchema);
+
+// export default GroceryList;
