dd2e6ca55b3102e7224681431acbf7c2d2becbd9
Kenji Watanabe <kenkowatanabe1504@gmail.com>
2025-09-27 23:25:06 +1000

    Finished UI styling for my add image feature. Added dummy local data for now in FridgeList.jsx file

--- frontend/src/components/DashboardComponent/AddButton.jsx
+++ frontend/src/components/DashboardComponent/AddButton.jsx
@@ -23,8 +23,8 @@ export default function AddButton() {
         <button onClick={handleAddIngredientOpen} className="fixed bottom-35 right-9.5 w-9 h-9 bg-[#36874D] text-white rounded-full shadow-lg flex items-center justify-center hover:bg-green-600 active:bg-green-700 transition-colors text-2xl">?</button>
       </>
       }
-      <button onClick={handleMainButtonClick} className="fixed bottom-6 right-6 w-16 h-16 bg-[#3DC581] text-white rounded-full shadow-lg flex items-center justify-center hover:bg-green-600 active:bg-green-700 transition-colors text-6xl">
-        +
+      <button onClick={handleMainButtonClick} className="fixed bottom-6 right-6 w-16 h-16 bg-[#3DC581] text-white rounded-full shadow-lg flex items-start justify-center hover:bg-green-600 active:bg-green-700 transition-colors text-6xl">
+         <span className="absolute -top-1 left-1/2 transform -translate-x-1/2 text-6xl">+</span>
       </button>
     </>
   )
